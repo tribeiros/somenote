a2b4205df88af0e1866894cc164ac836f14941d5
Tiago Ribeiro <tribeiros@indracompany.com>
2018-06-18 14:23:45 -0300

    notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -29,7 +29,7 @@ module.exports = function(app){
 
     noteDao.atualiza(note, function(erro){
       if (erro){
-        res.status(500).send(erro);
+       ce res.status(500).send(erro);
         return;
       }
       console.log('note cancelado.')
