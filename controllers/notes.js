e71c248934b97f22b579b696f44281384eb015f9
Tiago Ribeiro <tribeiros@indracompany.com>
2018-06-28 17:27:29 -0300

    up

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
   app.get('/notes', function(req, res){
     console.log('Recebida requisicao de teste na porta 3000.')
-    res.send('OK.');
+    res.send('Ok.');
   });
 
 // view all rows on notes databes
